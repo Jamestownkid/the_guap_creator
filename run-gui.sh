22fe47742ed2c15ccce598e73f809dfe4efa3db7
Jamestownkid <jamestownkid@users.noreply.github.com>
2025-11-27 15:18:59 -0500

    Fix JavaFX module path for Linux

--- run-gui.sh
+++ run-gui.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Launcher script that handles JavaFX rendering issues on Linux
 
-# Force JavaFX to use software rendering (fixes most Linux issues)
-export _JAVA_OPTIONS="-Djavafx.platform=gtk -Dprism.order=sw -Dprism.verbose=false"
-
 # Get the directory where this script is located
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Run the GUI
-java -jar "$DIR/target/autovideo-0.0.1-SNAPSHOT-gui.jar" "$@"
+# Run the GUI with JavaFX settings that fix graphics issues
+java \
+  -Djavafx.platform=gtk \
+  -Dprism.order=sw \
+  -Dprism.verbose=false \
+  -jar "$DIR/target/autovideo-0.0.1-SNAPSHOT-gui.jar" "$@"
 
